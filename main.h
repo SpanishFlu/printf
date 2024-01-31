7d5967731d4cc73bfee7010b31b118a6c0ec5c28
SpanishFlu <saifeddinesayed@gmail.com>
2024-01-31 09:32:33 -0800

    assashshdaad

--- main.h
+++ main.h
@@ -3,6 +3,11 @@
 
 #include <stdarg.h>
 
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(void);
+int print_int(va_list args);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
+
